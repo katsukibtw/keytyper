04c34f1a465f0253ded90e1c3297ee87e97b4b7d
katsuki <katsukibtw@yandex.ru>
2023-06-04 18:13:45 +0300

    some fixes

--- src/App.js
+++ src/App.js
@@ -43,8 +43,8 @@ import JoinRoom from './components/JoinRoom';
 import SocketContext from './SocketContext';
 import { io } from 'socket.io-client';
 // some minor configuration for hosting
-// export const host = 'http://94.181.190.26:9967';
-export const host = 'http://localhost:9967';
+export const host = 'http://94.181.190.26:9967';
+// export const host = 'http://localhost:9967';
 export const indexPath = "/10v/skripko/keytyper";
 
 const socket = io(host, { transports: ['websocket', 'polling', 'flashsocket'] });
